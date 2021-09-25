9e42653e763f1aebcda96f413a0e6f228348f7b9
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-25 21:02:46 +0300

    create models

---
+++ project/neighborhood/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from PIL import Image
+from django_countries.fields import CountryField
+
+
+# Create your models here.
