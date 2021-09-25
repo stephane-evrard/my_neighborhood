3980bf0408a6b6c1d603223dc80eaf1010a662bc
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-25 21:07:15 +0300

    class Business in model

--- project/neighborhood/models.py
+++ project/neighborhood/models.py
@@ -83,5 +83,55 @@ class Profile(models.Model):
     class Meta:
         verbose_name = 'Profile'
         verbose_name_plural = 'Profiles'
+
+class Business(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    Admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    admin_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True, default='1')
+    address = models.TextField()
+    neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE, blank=True, default='1')
+
+    
+    def save_business(self):
+        self.save()
+    
+    def delete_business(self):
+        self.delete()
+        
+    @classmethod
+    def get_allbusiness(cls):
+        business = cls.objects.all()
+        return business
+    
+    @classmethod
+    def search_business(cls, search_term):
+        business = cls.objects.filter(name__icontains=search_term)
+        return business
+    
+    @classmethod
+    def get_by_neighborhood(cls, neighborhoods):
+        business = cls.objects.filter(neighborhood__name__icontains=neighborhoods)
+        return business
+    
+    @classmethod
+    def get_businesses(request, id):
+        try:
+            business = Business.objects.get(pk = id)
             
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return business
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'My Business'
+        verbose_name_plural = 'Business'
+
+
  
