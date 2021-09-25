7c94cae235955ebf6831d5e6b5290a046a3b4b14
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-25 20:55:38 +0300

    class New Business Form

---
+++ project/neighborhood/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from registration.forms import RegistrationForm
+from crispy_forms.helper import FormHelper
+
+
+class NewBusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['Admin', 'pub_date', 'admin_profile']
+        widgets = {
+          'address': forms.Textarea(attrs={'rows':1, 'cols':10,}),
+        }
+
