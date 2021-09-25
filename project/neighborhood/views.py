5d48d9ae935c9fd674ac5d208de2538fb10893f8
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-25 21:14:22 +0300

    index view

--- project/neighborhood/views.py
+++ project/neighborhood/views.py
@@ -8,3 +8,26 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from .forms import *
 from django.contrib import messages
+
+# Create your views here.
+
+def index(request):
+    date = dt.date.today()
+    # business = Business.get_allbusiness()
+    all_neighborhoods = Neighborhood.get_neighborhoods()
+    
+    
+    if 'neighborhood' in request.GET and request.GET["neighborhood"]:
+        neighborhoods = request.GET.get("neighborhood")
+        searched_neighborhood = Business.get_by_neighborhood(neighborhoods)
+        all_posts = Posts.get_by_neighborhood(neighborhoods)
+        message = f"{neighborhoods}"
+        all_neighborhoods = Neighborhood.get_neighborhoods()        
+        
+        return render(request, 'index.html', {"message":message,"location": searched_neighborhood,
+                                               "all_neighborhoods":all_neighborhoods, "all_posts":all_posts})
+
+    else:
+        message = "No Neighborhood Found!"
+
+    return render(request, 'index.html', {"date": date, "all_neighborhoods":all_neighborhoods,})
