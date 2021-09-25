39610ad8a00e32a4c1ede4424125ff21265bcc04
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-25 21:06:08 +0300

    class Profile in model

--- project/neighborhood/models.py
+++ project/neighborhood/models.py
@@ -57,3 +57,31 @@ class Neighborhood(models.Model):
         ordering = ['-pub_date']
         verbose_name = 'My Neighborhood'
         verbose_name_plural = 'Neighborhoods'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = models.ImageField(upload_to = 'profile_pics/', blank=True, default='profile_pics/default.jpg')
+    neighborhood = models.ForeignKey(Neighborhood,on_delete=models.SET_NULL, blank=True, null=True)
+
+    def save_profile(self):
+        self.save()
+        
+        img = Image.open(self.photo.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.photo.path)
+            
+
+    def delete_profile(self):
+        self.delete()
+    
+    def __str__(self):
+        return f"{self.user}, {self.bio}, {self.photo}"
+    
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+            
+ 
