6abea919b9531c2da4ebef84fb17a162ec61b225
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-25 21:11:53 +0300

    urls

---
+++ project/neighborhood/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^$', views.index, name= 'index'),
+    url(r'^search/', views.search_businesses, name='search_results'),
+    url(r'^business/(\d+)', views.get_business, name='business_results'),
+    url(r'^new/business$', views.new_business, name='new-business'),
+    url(r'^new/post$', views.new_post, name='new-post'),
+    url(r'^accounts/profile/$', views.user_profiles, name='profile'),
+   
+
+    
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
